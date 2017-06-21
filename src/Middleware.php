5b2459be2e3ffc005878862b0431cd6a9a2f642a
Lorenzo Marzullo <lorenzo@MacBook-Pro-di-Lorenzo-3.local>
2017-06-21 08:58:26 +0200

    Updating the way used to parse header field according to the rfc7230 specifications

--- src/Middleware.php
+++ src/Middleware.php
@@ -26,8 +26,8 @@ class Middleware implements MiddlewareInterface
 
     public function process(ServerRequestInterface $request, DelegateInterface $delegate)
     {
-        if (!$this->isValidMediaType($request)) {
-            return new Response('php://memory', 415);
+        if (!$this->isValidContentType($request)) {
+            //return new Response('php://memory', 415);
         }
 
         if (!$this->containsValidAccept($request)) {
@@ -40,7 +40,7 @@ class Middleware implements MiddlewareInterface
     }
 
     /**
-     * isValidMediaType.
+     * isValidContentType.
      * Check if the first item in content-type header strictly match with allowed JSON API media type, without any extra
      * parameter.
      *
@@ -48,11 +48,9 @@ class Middleware implements MiddlewareInterface
      *
      * @return bool
      */
-    private function isValidMediaType(ServerRequestInterface $request)
+    private function isValidContentType(ServerRequestInterface $request)
     {
-        $header = array_shift($request->getHeader('content-type'));
-
-        return strtolower($header) === static::MEDIA_TYPE;
+        return array_shift($this->parseHeader($request, 'content-type')) === static::MEDIA_TYPE;
     }
 
     /**
@@ -81,5 +79,26 @@ class Middleware implements MiddlewareInterface
         }
 
         return !$hasMediaType;
+    }
+
+    /**
+     * parseHeader.
+     * Parse the header with the given name according to the specification:
+     * https://tools.ietf.org/html/rfc7230#section-3.2
+     *
+     * @param ServerRequestInterface $request
+     * @param string $headerName
+     *
+     * @return array
+     */
+    private function parseHeader(ServerRequestInterface $request, $headerName)
+    {
+        $values = [];
+
+        foreach ($request->getHeader($headerName) as $value) {
+            $values = array_merge($values, array_map('strtolower', array_map('trim', explode(',', $value))));
+        }
+
+        return $values;
     }
 }
